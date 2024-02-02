1ca7df086b9ad8216d3b0cbb1d5d789c68f0dce8
kartikrajput3073 <111582743+kartikrajput3073@users.noreply.github.com>
2024-02-02 22:46:11 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,10 +5,7 @@ import numpy as np
 
 
 
-import datetime
-from datetime import date, timedelta
-from statsmodels.tsa.seasonal import seasonal_decompose
-import statsmodels.api as sm
+
 
 app='stock market forecast'
 st.write(app)
