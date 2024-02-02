19e11940e646958ea94a775192a981f27c2f8822
kartikrajput3073 <111582743+kartikrajput3073@users.noreply.github.com>
2024-02-02 22:41:52 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import datetime
+from datetime import date, timedelta
+from statsmodels.tsa.seasonal import seasonal_decompose
+import statsmodels.api as sm
+
+app='stock market forecast'
+st.write(app)
 print("heello")
 
 
